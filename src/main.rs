0d68559ddf51bc7d1e33b3c380cbbd78586c2089
Christopher <ckaliszewski@gmail.com>
2025-03-15 04:43:44 -0400

    add reqwest library

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
+use std::error;
+
 /// Application entry point.
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn error::Error>> {
+    let ip_address = reqwest::get("https://api.ipify.org").await?.text().await?;
+
+    println!("Hello, world! My IP address is {}", ip_address);
+
+    Ok(())
 }
